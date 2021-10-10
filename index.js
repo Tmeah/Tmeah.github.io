5ecc2cbb73e46ccc4af868ccccddc75b07a116ec
Tmeah <tausifmeah@gmail.com>
2021-10-10 22:44:48 +0100

    ffa

---
+++ index.js
@@ -0,0 +1,11 @@
+function openHeader() {
+  document
+    .getElementById("header__anchor")
+    .classList.toggle("header__logo--popper1");
+}
+
+function stopOpac() {
+  document
+    .getElementById("header__logo--hover")
+    .classList.toggle("header__logo--hover");
+}
